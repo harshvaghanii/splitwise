4700b61158afb16e30e6a1f580f17210409a76fa
harshvaghanii <harshvaghani98@gmail.com>
2024-11-30 22:02:09 -0500

    updating the name of entities in db

--- src/main/java/com/cs597/project/splitwise/entities/BalanceEntity.java
+++ src/main/java/com/cs597/project/splitwise/entities/BalanceEntity.java
@@ -15,6 +15,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Getter
 @Setter
+@Table(name = "balances")
 public class BalanceEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/cs597/project/splitwise/entities/TransactionEntity.java
+++ src/main/java/com/cs597/project/splitwise/entities/TransactionEntity.java
@@ -16,6 +16,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "transactions")
 public class TransactionEntity {
 
     @Id
